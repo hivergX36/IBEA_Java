0796b06e676905225809264f444acc33af5f2931
hivergX36 <vic.herve44@gmail.com>
2025-02-13 00:59:49 +0100

    add ressources_13_02_2025

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
+
+import com.Ibea;
+
 import java.util.*;
 import java.lang.reflect.*;
 import java.nio.file.Paths;
@@ -12,26 +15,16 @@ import java.nio.file.Path;
 import java.nio.file.LinkOption; 
 
 public class Main{
- 
+
 public static void main(String[] args) throws IOException {
- /*    Ressource ressource = new Ressource();
+   Ressource ressource = new Ressource();
     String path_file = ressource.getfileinressource("knapsack.txt");
     System.out.println(path_file);
 
-NSGAII NSGAIIAlgorithm = new NSGAII(path_file,50,20);
+Ibea IbeaAlgorithm = new Ibea(path_file,50,20);
 
-System.out.println(NSGAIIAlgorithm.NbVariable);
-NSGAIIAlgorithm.resolve(100);
-
-
-*/
-
-int t1[] = {1,2,3};
-int t2[] = new int[5];
-t2 = t1; 
-
-for (int i = 0; i < 3; i++)
-System.out.println(t2[i]);
+System.out.println(IbeaAlgorithm.NbVariable);
+IbeaAlgorithm.resolve(100);
 }
 }
 
